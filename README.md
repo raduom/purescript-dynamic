b9449beaef6a7bc988ef2d17b709da1aa1b17899
Radu Ometita <radu.ometita@gmail.com>
2017-01-26 23:36:53 +0200

    Add some documentation.

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 [![Latest release](http://img.shields.io/github/release/raduom/purescript-dynamic.svg)](https://github.com/raduom/purescript-dynamic/releases)
 [![Build status](https://travis-ci.org/raduom/purescript-dynamic.svg?branch=master)](https://travis-ci.org/raduom/purescript-dynamic)
 
+The Dynamic interface provides basic support for dynamic types.
+
+Operations for injecting values of arbitrary type into a dynamically typed value, Dynamic, are provided, together with operations for converting dynamic values into a concrete (monomorphic) type.
+
 ## Installation
 
 ```
--- src/Data/Dynamic.purs
+++ src/Data/Dynamic.purs
@@ -12,14 +12,18 @@ import Unsafe.Coerce (unsafeCoerce)
 
 foreign import data Any :: Type
 
+-- | The type used to encode dynamicaly typed objects
 data Dynamic = Dynamic TypeRep Any
 
 instance showDynamic :: Show Dynamic where
   show (Dynamic t _) = "<< " <> show t <> " >>"
 
+-- | Converts an arbitrary value into an object of type `Dynamic`
 toDynamic :: forall a. Typeable a => a -> Dynamic
 toDynamic v = Dynamic (typeOf (Proxy :: Proxy a)) (unsafeCoerce v)
 
+-- | Converts a `Dynamic` object back into an ordinary value of the
+-- | correct type.
 fromDynamic :: forall a. Typeable a => Dynamic -> Maybe a
 fromDynamic (Dynamic t v)
   | t == typeOf (Proxy :: Proxy a) = Just (unsafeCoerce v)
