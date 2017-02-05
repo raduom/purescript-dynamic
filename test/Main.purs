0e95b7c20ad6ff544a76825fcbe0030c111c708c
Radu Ometita <radu.ometita@gmail.com>
2017-02-05 12:55:01 +0200

    Add new test, after trying to make it work with a Typeable1 instance.

--- test/Main.purs
+++ test/Main.purs
@@ -1,15 +1,30 @@
 module Test.Main where
 
 import Control.Monad.Eff (Eff)
+import Data.Dynamic (fromDynamic, toDynamic)
 import Data.List (List(Nil))
 import Data.Maybe (Maybe(..))
-import Data.Typeable (class Typeable, TyCon(..), TypeRep(..))
-import Data.Dynamic (fromDynamic, toDynamic)
+import Data.Typeable (class Typeable, class Typeable1, TyCon(..), TypeRep(..))
 import Prelude (class Eq, class Show, Unit, show, (<>), (==), bind)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
+
+data LV a = LV Int
+          | LA a
+
+instance typeable1LV :: Typeable1 LV where
+  typeOf1 _ = TypeRep (TyCon { tyConModule: "Test.Main", tyConName: "LV" }) Nil
+
+instance eqLV :: (Eq a) => Eq (LV a) where
+  eq (LV u) (LV v) = u == v
+  eq (LA u) (LA v) = u == v
+  eq _ _ = false
+
+instance showLV :: (Show a) => Show (LV a) where
+  show (LV u) = "LV " <> show u
+  show (LA u) = "LA " <> show u
 
 data T = A String
        | B Int
@@ -46,6 +61,10 @@ someB = B 5
 someC :: C
 someC = C 42
 
+someLV :: LV String
+someLV = LA "something"
+
+
 main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] do
   describe "Dynamic" do
@@ -58,4 +77,12 @@ main = run [consoleReporter] do
       it "should return Nothing for any branch" do
         let dyn = toDynamic someC
         let cast = (fromDynamic dyn) :: Maybe T
+        cast `shouldEqual` Nothing
+    describe "Converting a Typeable1 to dyanmic and back" do
+      it "should return the original" do
+        fromDynamic (toDynamic someLV) `shouldEqual` Just someLV
+    describe "Converting a typeable to the wrong type" do
+      it "should return Nothing for any branch" do
+        let dyn = toDynamic someC
+        let cast = (fromDynamic dyn) :: Maybe (LV String)
         cast `shouldEqual` Nothing
